1f78e9d178a4c1ff990c1cbe0892d86f6b92816f
rezzouq <mohammed.rezzouq@siway.ma>
2022-06-29 15:55:41 +0100

    ci(jest with testing a sum function and render of component):

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ export default {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   moduleNameMapper: {
-    '///^.+.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '/^.+.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
